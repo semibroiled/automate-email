117136ba5c21cef9edf51def94c6008b52a5a223
semibroiled <amitavcmostafa@gmx.de>
2021-08-31 17:08:25 +0200

    Edit out hardcoded password

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+#Import relevant libraries and packages
 import pandas as pd
 import smtplib
 from email.mime.text import MIMEText
+#from email.MIMEMultipart import MIMEMultipart
 import credentials
 
-'''my_mail = 'your email'
+'''
+credentials.py
 
-my_pass = 'your password'
+my_mail = 'your email'
+
+my_pass = input('Enter your password:\n') #Will ask for password after you import credentials
 
 my_smtp = 'mail.gmx.net'
 
@@ -25,24 +30,34 @@ message = f\'\'\'Sehr geehrter {Anrede} {Name},\n\nhiermit möchte ich mich für
 
 #! ONLY WORKS IF YOU HAVE NO TWO FACTOR AUTHENTICATION!!!
 
-try:
-    message = credentials.message
-    msg = MIMEText(message)
-    msg['Subject'] = credentials.subject
-    msg['From'] = credentials.my_mail
-    msg['To'] = credentials.your_mail
+#Initialize Message Body
+message = credentials.message
+msg = MIMEText(message)
+msg['Subject'] = credentials.subject
+msg['From'] = credentials.my_mail
+msg['To'] = credentials.your_mail
 
+try:
+
+    #Connect to SMTP i.e. GMX Server
     print('Starting Process')
     smtpObj = smtplib.SMTP(credentials.my_smtp, credentials.my_port)
     print('SMTP Object Made')
     smtpObj.ehlo()
     smtpObj.starttls()
+    
+    #Login
     print('Logging in...')
     smtpObj.login(credentials.my_mail, credentials.my_pass)
     print('Logged in!')
+    
+    #Send email
     print('Sending Email...')
     smtpObj.sendmail(credentials.my_mail, credentials.your_mail, msg.as_string())
     print('Email Sent!')
     smtpObj.quit()
 except smtplib.SMTPDataError as de:
     print(de)
+
+if __name__ == '__main__':
+    pass
