6e9b93712f799cf7f82702ccf9776472f1107684
semibroiled <amitavcmostafa@gmx.de>
2021-09-02 03:13:58 +0200

    Fix logicflow

--- os_test.py
+++ os_test.py
@@ -1,3 +1,15 @@
 import os
 
-print(os.)
+print(os.listdir())
+cwd = os.getcwd()
+subdirs = []
+for directory in os.listdir():
+    if '.csv' in directory:
+        filepath = cwd+'/'+directory
+        print(filepath)
+        full_path = os.path.join(cwd, directory)
+	
+    #if os.path.isdir(full_path): # i wonder what does this do. apparently shows folerds in this path ie deeper directories
+	#    subdirs.append(full_path)
+        
+print(subdirs)
